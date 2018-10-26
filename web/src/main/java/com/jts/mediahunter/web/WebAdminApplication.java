0d36c4b93d3b3bf312cd5f37325b95434e5882fe
tonysafarik <tony.safarik@seznam.cz>
2018-10-26 20:13:28 +0200

    thymeleaf running

--- web/src/main/java/com/jts/mediahunter/web/WebAdminApplication.java
+++ web/src/main/java/com/jts/mediahunter/web/WebAdminApplication.java
@@ -1,17 +1,18 @@
 package com.jts.mediahunter.web;
 
+import com.jts.mediahunter.core.CoreConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  *
- * @author Jan Tony Safarik
+ * @author Tony
  */
-@SpringBootApplication
+@SpringBootApplication(scanBasePackageClasses = {WebConfiguration.class, CoreConfiguration.class})
 public class WebAdminApplication {
-    
+
     public static void main(String[] args) {
         SpringApplication.run(WebAdminApplication.class, args);
     }
-    
+
 }
