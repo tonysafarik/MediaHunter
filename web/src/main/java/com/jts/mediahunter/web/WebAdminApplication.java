680b286a5b4e4690c67f303ddcedc12761e33e01
tonysafarik <tony.safarik@seznam.cz>
2018-10-26 10:57:28 +0200

    added Main method

---
+++ web/src/main/java/com/jts/mediahunter/web/WebAdminApplication.java
@@ -0,0 +1,17 @@
+package com.jts.mediahunter.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ * @author Jan Tony Safarik
+ */
+@SpringBootApplication
+public class WebAdminApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(WebAdminApplication.class, args);
+    }
+    
+}
