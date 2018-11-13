57923ca7e4a6e3dae5cebafb18f0f9b7b6dd3446
tonysafarik <tony.safarik@seznam.cz>
2018-11-13 21:22:48 +0100

    find record by external ID in DB

--- core/src/main/java/com/jts/mediahunter/core/dao/RecordDAO.java
+++ core/src/main/java/com/jts/mediahunter/core/dao/RecordDAO.java
@@ -3,6 +3,7 @@ package com.jts.mediahunter.core.dao;
 import com.jts.mediahunter.core.dao.recordstages.AcceptedStage;
 import com.jts.mediahunter.core.dao.recordstages.RejectedStage;
 import com.jts.mediahunter.domain.entities.Record;
+import java.util.List;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 /**
@@ -10,5 +11,12 @@ import org.springframework.data.mongodb.repository.MongoRepository;
  * @author Tony
  */
 public interface RecordDAO extends MongoRepository<Record, String>, AcceptedStage, RejectedStage {
+    
+    /**
+     * finds Records ONLY in waiting stage!
+     * @param externalId
+     * @return 
+     */
+    public List<Record> findByExternalId(String externalId);
     
 }
