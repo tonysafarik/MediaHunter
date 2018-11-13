b95a30af77b87388a44262ac692777da11fe17bc
tonysafarik <tony.safarik@seznam.cz>
2018-11-13 21:23:32 +0100

    accepting waiting and rejected records definition

--- core/src/main/java/com/jts/mediahunter/core/dao/recordstages/AcceptedStage.java
+++ core/src/main/java/com/jts/mediahunter/core/dao/recordstages/AcceptedStage.java
@@ -1,9 +1,15 @@
 package com.jts.mediahunter.core.dao.recordstages;
 
+import com.jts.mediahunter.domain.entities.Record;
+
 /**
  *
  * @author Tony
  */
 public interface AcceptedStage {
+    
+    public void acceptWaitingRecord(Record record);
+    
+    public void acceptRejectedRecord(Record record);
     
 }
