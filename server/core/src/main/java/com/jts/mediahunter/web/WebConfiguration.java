1594a3844d21d8013b4f07d2596c6b30b59eb518
Jan Tony Safarik <tony.safarik@seznam.cz>
2020-01-22 20:19:49 +0100

    Remove web security and core tests

--- server/core/src/main/java/com/jts/mediahunter/web/WebConfiguration.java
+++ server/core/src/main/java/com/jts/mediahunter/web/WebConfiguration.java
@@ -1,42 +1,13 @@
 package com.jts.mediahunter.web;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 /**
  * @author Jan Tony Safarik
  */
 @Configuration
 @EnableScheduling
-@EnableWebSecurity
-public class WebConfiguration extends WebSecurityConfigurerAdapter {
-
-    @Override
-    protected void configure(final AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("admin").password(encoder().encode("admin")).roles("ADMIN");
-    }
-
-    @Bean
-    public PasswordEncoder encoder() {
-        return new BCryptPasswordEncoder();
-    }
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.cors().and().csrf().disable().authorizeRequests()
-                .antMatchers("/login").authenticated()
-                .antMatchers("/channel","/channel/**").authenticated()
-                .antMatchers("/multimedium","/multimedium/**").authenticated()
-                .and()
-                .httpBasic();
-    }
+public class WebConfiguration {
 
 }
